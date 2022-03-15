2e9547c689b5acd6a07da15b92038a5234562818
DeepikaAnbalagan <92775307+DeepikaAnbalagan@users.noreply.github.com>
2022-03-15 18:16:30 +0530

    second_commit

---
+++ wordpress/wp-content/themes/twentytwentyone-child/Contact.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* Template Name:Contact
+* Template Post Type: post, page
+*
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+get_footer();
+?>
