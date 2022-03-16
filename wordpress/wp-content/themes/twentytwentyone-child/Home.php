59758eb27b84863cbe7e926511da004cbf7ea5d3
DeepikaAnbalagan <92775307+DeepikaAnbalagan@users.noreply.github.com>
2022-03-16 18:22:41 +0530

    new changes

--- wordpress/wp-content/themes/twentytwentyone-child/Home.php
+++ wordpress/wp-content/themes/twentytwentyone-child/Home.php
@@ -31,19 +31,38 @@ echo do_shortcode('[smartslider3 slider="2"]');
 			  <div class="section-title features-title heading">Why choose us</div>
 			</div>		
 		 <div class="row" id="feat">
+		 <?php 
+                   $args = array(  
+                    'post_type' => 'chooseus',
+                    'post_status' => 'publish',
+                    'order' => 'ASC',
+                    'posts_per_page' => 6
+                  
+     );
+            
+                $loop = new WP_Query( $args ); 
+                    
+                while ( $loop->have_posts() ) : $loop->the_post(); 
+               ?>
 			<div class="column col-sm-12  col-md-6 col-lg-3">
 				<div class="feature" data-animate-in="up">
 	               <div class="feature-image primary-color">
 	            	<div class="feature-icon"><span style="font-family:'fontawesome'">&#xf109</span>
 				</div>			
 			</div>
-	        <h5 class="feature-title"> Responsive Design	</h5>
+	        <h5 class="feature-title"><?php the_field('feature_title');?>	</h5>
 		<div class="feature-content">
-			<p>Etiam nec odio vestibulum est mattis effic iturut magna. Pellentesque sit am.</p>
+			<p><?php the_field('feature_content');?></p>
 		</div>
    </div>
 </div>
-  <div class="column col-sm-12 col-lg-3 col-md-6">
+<?php
+      endwhile;
+      
+            wp_reset_postdata(); 
+               
+      ?>
+  <!-- <div class="column col-sm-12 col-lg-3 col-md-6">
 	<div class="feature" data-animate-in="up">
 	   <div class="feature-image primary-color">
 		 <div class="feature-icon"><span style="font-family:'fontawesome'">&#xf0f4</span></div>			</div>
@@ -73,19 +92,277 @@ echo do_shortcode('[smartslider3 slider="2"]');
 		<p>Unlimited flexibility with our homepage design. Try the best.</p>
 			</div>
        </div>
-     </div>
+     </div> -->
    </div>	
  </div>
 </div>
 
+<!-- //about section -->
+<div id="about" class="section about">
+	<div class="container">
+		<div class=" section-heading about-heading">
+			<div id="about-subtitle" class="about-subtitle section-subtitle">who we are</div>
+			<div class="section-title about-title heading">About us</div>
+		</div>			
+			<div class="row">
+			<?php 
+                   $args = array(  
+                    'post_type' => 'aboutus',
+                    'post_status' => 'publish',
+                    'order' => 'ASC',
+                    'posts_per_page' => 6
+                  
+     );
+            
+                $loop = new WP_Query( $args ); 
+                    
+                while ( $loop->have_posts() ) : $loop->the_post(); 
+               ?>
+				<div class="column col-lg-4 col-md-4">
+		
+					<div class="about__content"data-animate-in="up">
+		            	 <a href="#" title="Our Story">
+			                     <img  src="<?php the_field('about_image'); ?>" width="500" height="427" class="about__image wp-post-image" alt=""/>	
+					 	</a>
+	         	<h3 class="about__title">
+	               	<a href="https://www.demos.machothemes.com/antreas/our-story/" title="Our Story"><?php the_field('about_title'); ?></a>
+	           </h3>
+	   <p class="text-about"><?php the_field('about_us_content'); ?></p>
+	</div>
+</div>
+
+<?php
+      endwhile;
+      
+            wp_reset_postdata(); 
+               
+      ?>
+  </div>		
+ </div>
+</div>
+<!-- //about section end -->
 
 
 
 
+<!-- 
+//tagline section -->
+<div id="tagline" class="section tagline dark">
+	<div class="layer">
+	<div class="container">
+		<div class="tagline-body">
+			<div class="tagline-title"data-animate-in="fadeIn"><?php the_field('tagline_body');?></div>			
+      <div class="tagline-content" data-animate-in="fadeIn"><?php the_field('tagline_content'); ?></div>
+      			<a class="tagline-link button button-medium" href="#features">Learn More</a>
+          		</div>
+		</div>
+		</div>
+  </div>
+
+<!-- //end -->
 
 
 
+<!-- SERVICE FORM SECTION START -->
 
+<div id="services" class="section services">
+	<div class="container">
+            <div class=" section-heading services-heading">
+                <div id="services-subtitle" class="services-subtitle section-subtitle">our services</div>
+                   <div class="section-title services-title heading">What we can do for you</div>
+           </div>		
+            <div class="row">
+			<?php 
+                   $args = array(  
+                    'post_type' => 'services',
+                    'post_status' => 'publish',
+                    'order' => 'ASC',
+                    'posts_per_page' => 6
+                    );
+                $loop = new WP_Query( $args ); 
+               while ( $loop->have_posts() ) : $loop->the_post(); 
+               ?>
+                <div class="sec-service col-lg-4">
+                     <div class="service-col">
+                       <div class="service"data-animate-in="up">
+                            <a href="https://www.demos.machothemes.com/antreas/service/premium-consulting/">
+                             <div class="primary-color service-icon">
+                               
+                               <img src="<?php the_field('service_image');?>" class="primary-color">
+                              </div>
+                            </a>
+                       </div>
+                        <div class="service-body">
+                            <h4 class="service-title" data-animate-in="up">
+                            <a href="https://www.demos.machothemes.com/antreas/service/premium-consulting/"><?php the_field('service_body'); ?></a>
+                            </h4>
+	
+                             <div class="service-content"data-animate-in="fadeIn">
+                                <p><?php the_field('service_content'); ?></p>
+                            </div>
+		        	    </div>
+                     </div>
+                  </div>
+		<?php
+            endwhile;
+            wp_reset_postdata(); 
+         ?>
+                
+		</div>
+           </div>
+     </div>
+</div>
+
+
+<!-- SERVICE FORM SECTION END -->
+
+
+ 
+ <!-- TESTIMONIALS FORM SECTION START -->
+ <div id="testimonials" class="section testimonials">
+   	  <div class="container">
+		     <div class=" section-heading testimonials-heading">
+                    <div id="testimonials-subtitle" class="testimonials-subtitle section-subtitle">our testimonials</div>
+                    <div class="section-title testimonials-title heading">What people say about us</div>
+             </div>
+
+            
+             <div class="row"data-animate-in="up">
+			 <?php 
+                   $args = array(  
+                    'post_type' => 'testimonials',
+                    'post_status' => 'publish',
+                    'order' => 'ASC',
+                    'posts_per_page' => 6
+                    );
+                $loop = new WP_Query( $args ); 
+               while ( $loop->have_posts() ) : $loop->the_post(); 
+               ?>
+                       <div class="testi col-lg-4 ">
+                           <div class="testimonial">
+                                   <div class="testimonial-content">
+                                        <p data-animate-in="fadeIn"> <?php the_field('testimonial_content');?>
+                                        </p>
+                                    </div>
+                                       <img src="<?php the_field('testimonial_image');?>" class="testimonial-image">
+                                       <h4 class="testimonial-name"><?php the_field('testimonial_name');?></h4>
+                                       <div class="testimonial-position"><?php the_field('testimonial_position');?> </div>
+                                 </div>
+                             </div>
+         <?php
+            endwhile;
+            wp_reset_postdata(); 
+         ?>
+                     
+
+		        </div>
+             </div>
+       </div>
+   </div>
+
+<!-- TESTIMONIALS FORM SECTION END -->
+
+
+<!-- our Team -->
+<div id="team" class="section team">
+		     <div class="container">
+			         <div class=" section-heading team-heading">
+                           <div id="team-subtitle" class="team-subtitle section-subtitle">we are dedicated</div>
+                           <div class="section-title team-title heading">Meet our team</div>
+                     </div>	
+         <div class="row">
+		 <?php 
+                   $args = array(  
+                    'post_type' => 'ourteam',
+                    'post_status' => 'publish',
+                    'order' => 'ASC',
+                    'posts_per_page' => 6
+                    );
+                $loop = new WP_Query( $args ); 
+               while ( $loop->have_posts() ) : $loop->the_post(); 
+               ?>
+             <div class="column column-narrow col-sm-12 col-md-6 col-lg-3">
+                <div class="team-member" data-animate-in="up">
+                	<img  src="<?php  the_field('team_image'); ?>" class="team-member-image wp-post-image"/>
+                  	        <div class="team-member-body">
+							  <h3 class="team-member-title"><?php the_field('team_member_title'); ?></h3>
+		                       <div class="team-member-description"><?php the_field('team_member_description'); ?></div>
+	                            	<div class="team-member-content">
+	                            		<p><?php the_field('team_member_content');?></p>
+	                           	     </div>
+										<div class="team-member-links">
+												<div class="clear">
+												</div>
+										</div>	
+
+                        	</div>
+
+                </div>
+           </div>
+		   <?php
+            endwhile;
+            wp_reset_postdata(); 
+         ?>
+                 
+</div>	
+		
+             	
+	</div>
+ </div>
+
+
+ <!-- ourteam end -->
+
+ 
+<!-- CONTACT FORM SECTION START -->
+
+<div id="contact" class="section contact">
+	<div class="container">
+		<div class=" section-heading contact-heading">
+               <div id="contact-subtitle" class="contact-subtitle section-subtitle">get in touch</div>
+               <div class="section-title contact-title heading">Contact us</div>
+        </div>
+	    <div class="row">
+       
+                   <div class="col-lg-6 contact-details  contact__content" data-animate-in="up">
+				           <h5 class="contact-head">
+                      We’re Here To Help!</h5>
+                        <p class="contact-ad">Glider Technologies is designed to help your business safely transit into the digital age.</p>
+                        <h5 class="contact-head">Send us a message and we will show you how.</h5>
+                        <p class="contact-address"><i class="fa fa-map-marker"></i>&nbsp;&nbsp;<strong>Address:</strong>D2, No 32,33, Adhisri Apartments, Kothari Road, Nungambakkam,
+                          Chennai – 600034</p>
+                        <p class="contact-address"><i class="fa fa-phone"></i>&nbsp;&nbsp;<strong>Telephone:</strong> (+91) 908-739-4980</p>
+                        <p class="contact-address"><i class="fa fa-envelope"></i>&nbsp;&nbsp;<strong>Email:</strong> <a href="mailto:contact@gmail.com">famysys.com</a></p>
+                     </div>  
+           
+                    <div class="col-lg-6 con-form  contact__form"data-animate-in="up">
+                        <form>
+                            <div class="form-group">
+                                <label for="formGroupExampleInput" class="con-label">Full Name</label>
+                                <input type="text" class="form-controll" id="formGroupExampleInput" >
+                            </div>
+                            <div class="form-group">
+                                <label for="formGroupExampleInput2"  class="con-label">Company</label>
+                                <input type="text" class="form-controll" id="formGroupExampleInput2" >
+                            </div>
+                            <div class="form-group">
+                                <label for="formGroupExampleInput2"  class="con-label">Email  Address</label>
+                                <input type="text" class="form-controll" id="formGroupExampleInput2" >
+                            </div>
+                            <div class="form-group">
+                                <label for="formGroupExampleInput2" class="con-label" > Your Message</label>
+                                <textarea class="form-controll" id="formGroupExampleInput2"  rows="8"></textarea>
+                            </div>
+                            <input type="submit" value="Send message" class="cn-submit">
+                            </form>
+           
+                        </div>
+              </div>
+
+       
+ </div>
+</div>
+<!-- CONTACT FORM SECTION END -->
 
 
 
--- wordpress/wp-content/themes/twentytwentyone-child/functions.php
+++ wordpress/wp-content/themes/twentytwentyone-child/functions.php
@@ -29,52 +29,264 @@ add_action( 'widgets_init', 'wpb_widgets_init' );
 
 
 
-//  // Custom Post Type
-//  function chooseus_init() {
-//    // set up product labels
-//    $labels = array(
-//        'name' => 'chooseus',
-//        'singular_name' => 'chooseus',
-//        'add_new' => 'Add New chooseus',
-//        'add_new_item' => 'Add Newchooseus',
-//        'edit_item' => 'Edit chooseus',
-//        'new_item' => 'New chooseus',
-//        'all_items' => 'All chooseus',
-//        'view_item' => 'View chooseus',
-//        'search_items' => 'Search chooseus',
-//        'not_found' =>  'No chooseus Found',
-//        'not_found_in_trash' => 'No chooseus found in Trash', 
-//        'parent_item_colon' => '',
-//        'menu_name' => 'chooseus',
-//    );
+ // Feature section Custom Post Type
+ function chooseus_init() {
+   // set up product labels
+   $labels = array(
+       'name' => 'chooseus',
+       'singular_name' => 'chooseus',
+       'add_new' => 'Add New chooseus',
+       'add_new_item' => 'Add Newchooseus',
+       'edit_item' => 'Edit chooseus',
+       'new_item' => 'New chooseus',
+       'all_items' => 'All chooseus',
+       'view_item' => 'View chooseus',
+       'search_items' => 'Search chooseus',
+       'not_found' =>  'No chooseus Found',
+       'not_found_in_trash' => 'No chooseus found in Trash', 
+       'parent_item_colon' => '',
+       'menu_name' => 'chooseus',
+   );
    
-//    // register post type
-//    $args = array(
-//        'labels' => $labels,
-//        'public' => true,
-//        'has_archive' => true,
-//        'show_ui' => true,
-//        'capability_type' => 'post',
-//        'hierarchical' => false,
-//        'rewrite' => array('slug' => 'chooseus'),
-//        'query_var' => true,
-//        'menu_icon' => 'dashicons-editor-customchar',
-//        'supports' => array(
-//            'title',
-//            'editor',
-//            'excerpt',
-//            'trackbacks',
-//            'custom-fields',
-//            'comments',
-//            'revisions',
-//            'thumbnail',
-//            'author',
-//            'page-attributes'
-//        )
-//    );
-//    register_post_type( 'chooseus', $args );
+   // register post type
+   $args = array(
+       'labels' => $labels,
+       'public' => true,
+       'has_archive' => true,
+       'show_ui' => true,
+       'capability_type' => 'post',
+       'hierarchical' => false,
+       'rewrite' => array('slug' => 'chooseus'),
+       'query_var' => true,
+       'menu_icon' => 'dashicons-editor-customchar',
+       'supports' => array(
+           'title',
+           'editor',
+           'excerpt',
+           'trackbacks',
+           'custom-fields',
+           'comments',
+           'revisions',
+           'thumbnail',
+           'author',
+           'page-attributes'
+       )
+   );
+   register_post_type( 'chooseus', $args );
    
-//    // register taxonomy
-//    register_taxonomy('chooseus_category', 'chooseus', array('hierarchical' => true, 'label' => 'category', 'query_var' => true, 'rewrite' => array( 'slug' => 'chooseus-category' )));
-// }
-// add_action( 'init', 'chooseus_init' );
+   // register taxonomy
+   register_taxonomy('chooseus_category', 'chooseus', array('hierarchical' => true, 'label' => 'category', 'query_var' => true, 'rewrite' => array( 'slug' => 'chooseus-category' )));
+}
+add_action( 'init', 'chooseus_init' );
+//end
+
+
+
+
+// Aboutus section Custom Post Type
+function aboutus_init() {
+   // set up product labels
+   $labels = array(
+       'name' => 'aboutus',
+       'singular_name' => 'aboutus',
+       'add_new' => 'Add New aboutus',
+       'add_new_item' => 'Add New aboutus',
+       'edit_item' => 'Edit aboutus',
+       'new_item' => 'New aboutus',
+       'all_items' => 'All aboutus',
+       'view_item' => 'View aboutus',
+       'search_items' => 'Search aboutus',
+       'not_found' =>  'No aboutus Found',
+       'not_found_in_trash' => 'No aboutus found in Trash', 
+       'parent_item_colon' => '',
+       'menu_name' => 'aboutus',
+   );
+   
+   // register post type
+   $args = array(
+       'labels' => $labels,
+       'public' => true,
+       'has_archive' => true,
+       'show_ui' => true,
+       'capability_type' => 'post',
+       'hierarchical' => false,
+       'rewrite' => array('slug' => 'aboutus'),
+       'query_var' => true,
+       'menu_icon' => 'dashicons-editor-customchar',
+       'supports' => array(
+           'title',
+           'editor',
+           'excerpt',
+           'trackbacks',
+           'custom-fields',
+           'comments',
+           'revisions',
+           'thumbnail',
+           'author',
+           'page-attributes'
+       )
+   );
+   register_post_type( 'aboutus', $args );
+   
+   // register taxonomy
+   register_taxonomy('aboutus_category', 'aboutus', array('hierarchical' => true, 'label' => 'category', 'query_var' => true, 'rewrite' => array( 'slug' => 'aboutus-category' )));
+}
+add_action( 'init', 'aboutus_init' );
+//end
+
+// services section Custom Post Type
+function services_init() {
+   // set up product labels
+   $labels = array(
+       'name' => 'services',
+       'singular_name' => 'services',
+       'add_new' => 'Add New services',
+       'add_new_item' => 'Add New services',
+       'edit_item' => 'Edit services',
+       'new_item' => 'New services',
+       'all_items' => 'All services',
+       'view_item' => 'View services',
+       'search_items' => 'Search services',
+       'not_found' =>  'No services Found',
+       'not_found_in_trash' => 'No services found in Trash', 
+       'parent_item_colon' => '',
+       'menu_name' => 'services',
+   );
+   
+   // register post type
+   $args = array(
+       'labels' => $labels,
+       'public' => true,
+       'has_archive' => true,
+       'show_ui' => true,
+       'capability_type' => 'post',
+       'hierarchical' => false,
+       'rewrite' => array('slug' => 'services'),
+       'query_var' => true,
+       'menu_icon' => 'dashicons-editor-customchar',
+       'supports' => array(
+           'title',
+           'editor',
+           'excerpt',
+           'trackbacks',
+           'custom-fields',
+           'comments',
+           'revisions',
+           'thumbnail',
+           'author',
+           'page-attributes'
+       )
+   );
+   register_post_type( 'services', $args );
+   
+   // register taxonomy
+   register_taxonomy('services_category', 'services', array('hierarchical' => true, 'label' => 'category', 'query_var' => true, 'rewrite' => array( 'slug' => 'services-category' )));
+}
+add_action( 'init', 'services_init' );
+
+
+//end
+
+
+//testimonial section Custom Post Type
+function testimonials_init() {
+   // set up product labels
+   $labels = array(
+       'name' => 'testimonials',
+       'singular_name' => 'testimonials',
+       'add_new' => 'Add New testimonials',
+       'add_new_item' => 'Add New testimonials',
+       'edit_item' => 'Edit testimonials',
+       'new_item' => 'New testimonials',
+       'all_items' => 'All testimonials',
+       'view_item' => 'View testimonials',
+       'search_items' => 'Search testimonials',
+       'not_found' =>  'No testimonials Found',
+       'not_found_in_trash' => 'No testimonials found in Trash', 
+       'parent_item_colon' => '',
+       'menu_name' => 'testimonials',
+   );
+   
+   // register post type
+   $args = array(
+       'labels' => $labels,
+       'public' => true,
+       'has_archive' => true,
+       'show_ui' => true,
+       'capability_type' => 'post',
+       'hierarchical' => false,
+       'rewrite' => array('slug' => 'testimonials'),
+       'query_var' => true,
+       'menu_icon' => 'dashicons-editor-customchar',
+       'supports' => array(
+           'title',
+           'editor',
+           'excerpt',
+           'trackbacks',
+           'custom-fields',
+           'comments',
+           'revisions',
+           'thumbnail',
+           'author',
+           'page-attributes'
+       )
+   );
+   register_post_type( 'testimonials', $args );
+   
+   // register taxonomy
+   register_taxonomy('testimonials_category', 'testimonials', array('hierarchical' => true, 'label' => 'category', 'query_var' => true, 'rewrite' => array( 'slug' => 'testimonials-category' )));
+}
+add_action( 'init', 'testimonials_init' );
+
+//end
+
+
+//ourteam section Custom Post Type
+function ourteam_init() {
+   // set up product labels
+   $labels = array(
+       'name' => 'ourteam',
+       'singular_name' => 'ourteam',
+       'add_new' => 'Add New ourteam',
+       'add_new_item' => 'Add New ourteam',
+       'edit_item' => 'Edit ourteam',
+       'new_item' => 'New ourteam',
+       'all_items' => 'All ourteam',
+       'view_item' => 'View ourteam',
+       'search_items' => 'Search ourteam',
+       'not_found' =>  'No ourteam  Found',
+       'not_found_in_trash' => 'No ourteam found in Trash', 
+       'parent_item_colon' => '',
+       'menu_name' => 'ourteam',
+   );
+   
+   // register post type
+   $args = array(
+       'labels' => $labels,
+       'public' => true,
+       'has_archive' => true,
+       'show_ui' => true,
+       'capability_type' => 'post',
+       'hierarchical' => false,
+       'rewrite' => array('slug' => 'ourteam'),
+       'query_var' => true,
+       'menu_icon' => 'dashicons-editor-customchar',
+       'supports' => array(
+           'title',
+           'editor',
+           'excerpt',
+           'trackbacks',
+           'custom-fields',
+           'comments',
+           'revisions',
+           'thumbnail',
+           'author',
+           'page-attributes'
+       )
+   );
+   register_post_type( 'ourteam', $args );
+   
+   // register taxonomy
+   register_taxonomy('ourteam_category', 'ourteam', array('hierarchical' => true, 'label' => 'category', 'query_var' => true, 'rewrite' => array( 'slug' => 'ourteam-category' )));
+}
+add_action( 'init', 'ourteam_init' );
